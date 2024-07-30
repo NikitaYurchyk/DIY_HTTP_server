65db8f0d3d80ed8057d216b903da62e6b07407fa
Nikita Yurchyk <nikitayurchyk@MacBook-Pro-Nikita.local>
2024-07-31 00:10:45 +0200

    Extract URL path

--- src/Server.cs
+++ src/Server.cs
@@ -8,10 +8,22 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-var msg = Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n");
+var successMsg = Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n");
+var failMsg = Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\r\n\r\n");
+var client = server.AcceptSocket(); 
+byte[] buffer = new byte[1024];
+var recMsg = client.Receive(buffer);
+string data = Encoding.UTF8.GetString(buffer);
 
-var client = server.AcceptSocket(); // wait for client
-client.Send(msg);
+if (data[5] != data.IndexOf('/'))
+{
+ client.Send(failMsg);
+}
+else
+{
+ client.Send(successMsg);
+}
+
 client.Shutdown(SocketShutdown.Both);
 client.Close();
 
