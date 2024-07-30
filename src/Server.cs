d4b5e992c24ebe042818a52e44a648dae54bdbdf
Nikita Yurchyk <nikitayurchyk@MacBook-Pro-Nikita.local>
2024-07-30 23:29:46 +0200

    200 response is done

--- src/Server.cs
+++ src/Server.cs
@@ -8,7 +8,7 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-var msg = Encoding.UTF8.GetBytes(@"HTTP/1.1 200 OK\r\n\r\n");
+var msg = Encoding.ASCII.GetBytes(@"HTTP/1.1 200 OK\r\n\r\n");
 var client = server.AcceptSocket(); // wait for client
 client.Send(msg);
 
