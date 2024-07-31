61707116fc6e5f1b4a43be7cbdbf4aab7081ee06
Nikita Yurchyk <nikitayurchyk@MacBook-Pro-Nikita.local>
2024-07-31 04:39:12 +0200

    Respond with body

--- src/Server.cs
+++ src/Server.cs
@@ -8,26 +8,49 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-var successMsg = Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n");
+
 var failMsg = Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\r\n\r\n");
+
 
 var client = server.AcceptSocket(); 
 byte[] buffer = new byte[1024];
 var recMsg = client.Receive(buffer);
 string data = Encoding.UTF8.GetString(buffer);
-int indexOfSlash = data.IndexOf('/');
 
 
-
-if (data[indexOfSlash + 1] != ' ')
+string[] listOfWords = data.Split("\r\n");
+foreach (var word in listOfWords)
 {
- client.Send(failMsg);
+ Console.WriteLine(word);
+}
+
+
+
+
+var firstLine = listOfWords[0].Split(" ");
+if (firstLine[1] == "/")
+{
+ byte[] theWholeResp = Encoding.UTF8.GetBytes($"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n");
+ client.Send(theWholeResp);
+}
+else if (firstLine[1].Contains("/echo/"))
+{
+ int index = firstLine[1].IndexOf('o');
+ string body = "";
+ for (int i = index + 2; i < firstLine[1].Length; i++)
+ {
+  body += firstLine[1][i];
+ }
+
+ string resp = $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {body.Length}\r\n\r\n{body}";
+ byte[] encrypResp = Encoding.UTF8.GetBytes(resp);
+ client.Send(encrypResp);
 }
 else
 {
- client.Send(successMsg);
+ client.Send(failMsg);
 }
-// Console.WriteLine($"RECEIVED DATA: {data}");
+
 
 client.Shutdown(SocketShutdown.Both);
 client.Close();
